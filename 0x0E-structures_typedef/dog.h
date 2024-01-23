e75ec29e438fd7f09c90451fa2a7d1071eb3b543
Jibril050 <mohammedjibril050@gmail.com>
2024-01-23 04:49:58 -0800

    create a struct for a dog info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct - struct that create dog info
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: starting structures with alx
+ */
+struct dog
+{
+
+
+	char *name;
+	float age;
+	char *owner;
+
+
+};
+
+#endif
